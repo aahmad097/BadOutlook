70ff8f86013f142aba23b1c9e5a47ac4d62c5221
S4R1N <s4r1n97@gmail.com>
2021-02-22 17:35:35 -0500

    Add project files.

---
+++ BadOutlook/OutlookEmails.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Office.Interop.Outlook;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BadOutlook
+{
+    public class OutlookEmails
+    {
+        public string EmailFrom { get; set; }
+
+        public string EmailSubject { get; set; }
+
+        public string EmailBody { get; set; }
+
+        public static List<OutlookEmails> ReadMailItems()
+        {
+            Application outlookApplication = null;
+            NameSpace outlookNamespace = null;
+            MAPIFolder inboxFolder = null;
+
+            Items mailItems = null;
+            List<OutlookEmails> listEmailDetails = new List<OutlookEmails>();
+            OutlookEmails emailDetails;
+
+            try
+            {
+
+                outlookApplication = new Application();
+                outlookNamespace = outlookApplication.GetNamespace("MAPI");
+                inboxFolder = outlookNamespace.GetDefaultFolder(OlDefaultFolders.olFolderInbox);
+                mailItems = inboxFolder.Items;
+
+                foreach (MailItem item in mailItems)
+                {
+
+                    emailDetails = new OutlookEmails();
+                    emailDetails.EmailFrom = item.SenderEmailAddress;
+                    emailDetails.EmailSubject = item.Subject;
+                    emailDetails.EmailBody = item.Body;
+                    listEmailDetails.Add(emailDetails);
+                    ReleaseComObject(item);
+
+                }
+
+            }
+            catch (System.Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+
+            }
+            finally
+            {
+
+                ReleaseComObject(mailItems);
+                ReleaseComObject(inboxFolder);
+                ReleaseComObject(outlookNamespace);
+                ReleaseComObject(outlookApplication);
+
+            }
+
+            return listEmailDetails;
+
+        }
+
+        private static void ReleaseComObject(object obj)
+        {
+
+            if (obj != null)
+            {
+
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+
+            }
+
+        }
+
+    }
+}
---
+++ BadOutlook/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BadOutlook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mails = OutlookEmails.ReadMailItems();
+            int i = 1;
+
+            foreach (var mail in mails)
+            {
+                Console.WriteLine("Mail No " + i);
+                Console.WriteLine("Mail Recieved from " + mail.EmailFrom);
+                Console.WriteLine("Mail Subject " + mail.EmailSubject);
+                Console.WriteLine("Mail Body " + mail.EmailBody);
+                Console.WriteLine("");
+
+                i += 1;
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
